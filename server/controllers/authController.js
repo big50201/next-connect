57687ff27be3293d5a0e84d82478400890af4bb3
jennifer.sung <jennifer.sung@skylinetw.com>
2019-08-06 16:35:03 +0800

    登出及驗證Auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,6 +58,16 @@ exports.signin = (req,res,next) => {
 
 };
 
-exports.signout = () => {};
+exports.signout = (req,res) => {
+    res.clearCookie("next-cookie.sid");
+    req.logout();
+    res.json({message:"you are now sign out!"});
+};
 
-exports.checkAuth = () => {};
+exports.checkAuth = (req,res,next) => {
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    res.redirect("/signin");
+};
