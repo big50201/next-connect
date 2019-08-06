966be421269db0f3c9f799aa19f1f1f0f45853ed
jennifer.sung <jennifer.sung@skylinetw.com>
2019-08-06 16:44:45 +0800

    取得所有使用者

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,10 @@
-exports.getUsers = () => {};
+const monogoose = require("mongoose");
+const User = monogoose.model("User");
+
+exports.getUsers = async(req,res) => {
+    const users = await User.find().select("_id name email createAt updateAt");
+    res.json(users);
+};
 
 exports.getAuthUser = () => {};
 
